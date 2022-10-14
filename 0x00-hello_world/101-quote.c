48562f6d5445b239e963d9e3b8a5b56c72ef6238
karanimoseh <karanimoseh@gmail.com>
2022-10-14 15:02:37 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
-/**
- * main-runs teh main code
- *
- * Return: 1 if program completes
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",60);
-	return(1)
-}
+ /**
+   *main-runs teh main code
+   *
+   * Return: 1 if program completes
+   */
+ int main(void)
+ {
+	 write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 60);
+	 return (1) ;
+ }
